a9058303dccd8f5cdb8f7ac2432b4fdcd1b51107
Eric Sondergard <4650644+esond@users.noreply.github.com>
2021-11-03 16:05:32 -0600

    Add error handling controller and path to Web API template

---
+++ src/Templates/WebApi/Controllers/ErrorController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+namespace Xerris.WebApi1
+{
+    [ApiController]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class ErrorController : ControllerBase
+    {
+        [Route("/error-local-development")]
+        public IActionResult ErrorLocalDevelopment(
+            [FromServices] IWebHostEnvironment webHostEnvironment)
+        {
+            if (!webHostEnvironment.IsDevelopment())
+            {
+                throw new InvalidOperationException(
+                    "This shouldn't be invoked in non-development environments.");
+            }
+
+            var context = HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+            return Problem(
+                detail: context.Error.StackTrace,
+                title: context.Error.Message);
+        }
+
+        [Route("/error")]
+        public IActionResult Error() => Problem();
+    }
+}
--- src/Templates/WebApi/Startup.cs
+++ src/Templates/WebApi/Startup.cs
@@ -46,6 +46,7 @@ namespace Xerris.WebApi1
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseExceptionHandler("/Error");
             }
 
             app.UseHttpsRedirection();
