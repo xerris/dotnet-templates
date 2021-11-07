172df34f21292bcb13341beeb66d3dd540739f39
Eric Sondergard <4650644+esond@users.noreply.github.com>
2021-11-07 11:14:17 -0600

    Checkout project files that shouldn't get versioned before packing them
    This is a bit of a hack, it could cause legitimate changes to project files to be
    overwritten if there are uncommitted changes. But it should be good enough for now.

--- pack.ps1
+++ pack.ps1
@@ -1,9 +1,12 @@
-$solutionFile = ".\Xerris.Templates.sln"
-
 dotnet tool restore
 
 dotnet gitversion /updateprojectfiles /output buildserver
 
+# Only the parent project gets packaged, so only the parent project should get versioned.
+# See https://github.com/xerris/Xerris.DotNet.Templates/issues/5
+git checkout .\src\Templates\**\*.csproj -v
+
+$solutionFile = ".\Xerris.Templates.sln"
 $artifactsDirectory = "artifacts"
 
 Remove-Item  $artifactsDirectory -Force -Recurse -ErrorAction Ignore -Verbose
